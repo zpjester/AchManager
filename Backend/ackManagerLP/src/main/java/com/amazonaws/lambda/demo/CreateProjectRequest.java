c0fa6540eaa28c79664eede150e2f648ecc46f2f
bogenisgreat <bogenisgreat@gmail.com>
2021-12-01 04:35:27 -0500

    New AWS Lam Jav P

---
+++ Backend/ackManagerLP/src/main/java/com/amazonaws/lambda/demo/CreateProjectRequest.java
@@ -0,0 +1,11 @@
+package com.amazonaws.lambda.demo;
+
+public class CreateProjectRequest {
+
+	public String name;
+	
+	public CreateProjectRequest(String n) {
+		name = n;
+	}
+
+}
---
+++ Backend/ackManagerLP/src/main/java/com/amazonaws/lambda/demo/ProjectData.java
@@ -0,0 +1,55 @@
+package com.amazonaws.lambda.demo;
+public class ProjectData {
+    private String projectID;
+    private Teammate[] teammateList;
+    private Task[] taskList;
+    private boolean archived;
+    public int system = 200;
+
+	public ProjectData(String projectID, Teammate[] teammateList, Task[] taskList, boolean archived) {
+	    this.projectID = projectID;
+	    this.teammateList = teammateList;
+	    this.taskList = taskList;
+	    this.archived = archived;
+	}
+	public ProjectData(String projectID, Teammate[] teammateList, Task[] taskList, boolean archived, int s) {
+	    this.projectID = projectID;
+	    this.teammateList = teammateList;
+	    this.taskList = taskList;
+	    this.archived = archived;
+	    this.system = s;
+	}
+	
+	public String getProjectID() {
+	    return this.projectID;
+	}
+	
+	public void setProjectID(String newID) {
+	    this.projectID = newID;
+	}
+	
+	public Teammate[] getTeammateList() {
+	    return this.teammateList;
+	}
+	
+	public void setTeammateList(Teammate[] newTeammates) {
+	    this.teammateList = newTeammates;
+	}
+	
+	public Task[] getTaskList() {
+	    return this.taskList;
+	}
+	
+	public void setTaskList(Task[] newTasks) {
+	    this.taskList = newTasks;
+	}
+	
+	public boolean getArchived() {
+	    return this.archived;
+	}
+	
+	public void setArchived(boolean isArchived) {
+	    this.archived = isArchived;
+	}
+
+}
