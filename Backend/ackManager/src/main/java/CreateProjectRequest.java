9349141d434ceb87c2e981b7e455be73748db03a
bogenisgreat <bogenisgreat@gmail.com>
2021-12-01 04:09:40 -0500

    Progress on Lambda

--- Backend/ackManager/src/main/java/CreateProjectRequest.java
+++ Backend/ackManager/src/main/java/CreateProjectRequest.java
@@ -1,4 +1,10 @@
 
 public class CreateProjectRequest {
 
+	public String name;
+	
+	public CreateProjectRequest(String n) {
+		name = n;
+	}
+
 }
---
+++ Backend/ackManager/src/main/java/Project.java
@@ -0,0 +1,12 @@
+
+public class Project {
+	public String projectID;
+	public boolean isArchived = false;
+	public Project (String pID) {
+		projectID = pID;
+	}
+	public Project (String pID, boolean v) {
+		projectID = pID;
+		isArchived = v;
+	}
+}
