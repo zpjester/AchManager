50873a37a6b59077c9aeb990f24046cfa5f02f48
zpjester <58483011+zpjester@users.noreply.github.com>
2021-12-06 08:15:38 -0500

    Progress towards addTeammate

--- src/main/java/com/ach/lambda/demo/addTeammateRequest.java
+++ src/main/java/com/ach/lambda/demo/addTeammateRequest.java
@@ -4,8 +4,12 @@ public class addTeammateRequest {
 	String projectID;
 	String name;
 	
-	addTeammateRequest(String projectID, String name){
+	public addTeammateRequest(String projectID, String name){
 		this.projectID = projectID;
 		this.name = name;
+	}
+	public addTeammateRequest(){
+		this.projectID = "";
+		this.name = "";
 	}
 }
