ecb995c6e2c11f9dafe7b0003745a3311af39bdb
bogenisgreat <bogenisgreat@gmail.com>
2021-12-06 07:43:03 -0500

    AddTeam

--- src/main/java/com/ach/lambda/demo/TeamDAO.java
+++ src/main/java/com/ach/lambda/demo/TeamDAO.java
@@ -4,6 +4,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 public class TeamDAO {
 	java.sql.Connection conn;
@@ -36,5 +37,32 @@ public class TeamDAO {
 	private Teammate getTeammate(String name) {
 		
 		return new Teammate(name);
+	}
+	
+	private boolean addTeammate(String name, String p) throws SQLException {
+		
+		PreparedStatement ps = conn.prepareStatement("SELECT * FROM " + tblName1 + " WHERE Pid = ? and Name = ?;");
+        
+        ps.setString(1, p);
+        ps.setString(2, name);
+        
+        ResultSet resultSet = ps.executeQuery();
+        
+        // already present?
+        while (resultSet.next()) {
+        	//Project c = generateProject(resultSet);
+            resultSet.close();
+            return false;
+        }
+
+        ps = conn.prepareStatement("INSERT INTO " + tblName1 + " (MEMDERSid, Name, Pid) values(?,?);");
+        ps.setString(1, UUID.randomUUID().toString().replace("-", ""));
+        ps.setString(2, name);
+        ps.setString(3, p);
+//        ps.setBoolean(2,  constant.isArchived);
+        ps.execute();
+        return true;
+		
+		//return false;
 	}
 }
