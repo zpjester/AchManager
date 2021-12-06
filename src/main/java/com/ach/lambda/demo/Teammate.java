c9fdd5a070f99fad5126c22f6331d1c12a08f9f1
zpjester <58483011+zpjester@users.noreply.github.com>
2021-12-06 00:38:05 -0500

    JSONified a ton

--- src/main/java/com/ach/lambda/demo/Teammate.java
+++ src/main/java/com/ach/lambda/demo/Teammate.java
@@ -1,7 +1,13 @@
 package com.ach.lambda.demo;
+import org.json.simple.JSONObject;
 public class Teammate {
     String name;
     Teammate(String name){
     	this.name = name;
+    }
+    JSONObject toJSON() {
+    	JSONObject response = new JSONObject();
+    	response.put("name", name);
+    	return response;
     }
 }
